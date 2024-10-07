2b0b0a16287e54fb0d3b26853212dbe277bb2e5a
Andrei <andreihonorio@hotmsil.com>
2024-10-07 08:28:30 -0300

    atualização de metodos

---
+++ README.md
@@ -0,0 +1,3 @@
+# CursoPHP
+
+# Aqui tem um Class com criação da conecção com Mysql e outra class de select para otimazação do codigo.
--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <?php
 
 require_once("config.php");
+ #==========================================================================================================
 
-$sql = new Sql();
+#$sql = new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+#$usuarios = $sql->select("SELECT * FROM tb_usuarios"); //mostra todas as tabelas.
 
-echo json_encode($usuarios);
+#echo json_encode($usuarios);
 
+#===========================================================================================================
+
+#Carrega apenas um usuario
+
+//$consulta = new Usuario();
+
+//$consulta->loadById(2);
+
+//echo $consulta;
+
+#===========================================================================================================
+
+#Carrrega todos ususarios do banco
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+#===========================================================================================================
+
+#Carrega uma lista de usuarios buscando pelo login
+
+//$search = Usuario::search("all");
+
+//echo json_encode($search);
+
+#===========================================================================================================
+
+#Carrega um usuario com login e senha
+
+$usuario = new Usuario();
+$usuario->login("root","!@#$%");
+
+echo $usuario;
 ?>
